352f8e366bbeb520711235e3e8f3d79fa5264e13
unknown <nananono1829@gmail.com>
2025-05-05 12:49:36 +0900

    リザルトを表示(正答数をのぞく)

--- Assets/Scenes/result/ResultManager.cs
+++ Assets/Scenes/result/ResultManager.cs
@@ -6,10 +6,44 @@ using UnityEngine.SceneManagement;
 
 public class ResultManager : MonoBehaviour
 {
+    public Text Text_Letters_Sum;
+    public Text Text_Noise_Sum;
+    public Text Text_Score_Sum;
+    int heardCharsSum = 0;
+    int canceledNoiseSum = 0;
+    int correctAnswers; //正答数がsavedataから見れるようになってから
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Manager.Savedata data in Manager.savedata)
+        {
+            for(int i = 0; i < data.gotStatement.Length; i++){
+                if (data.gotStatement[i] != ' ') 
+                {
+                    heardCharsSum += 1;
+                }
+            }
+            canceledNoiseSum += data.canceledNoise;
+            //正答数がsavedataから見れるようになってから処理を追加
+        }
+        showHeardChars();
+        showCanceledNoise();
+        showCorrectAnswers();
+    }
+
+    void showHeardChars()
+    {
+        Text_Letters_Sum.text = heardCharsSum.ToString();
+    }
+
+    void showCanceledNoise()
+    {
+        Text_Noise_Sum.text = canceledNoiseSum.ToString();
+    }
+
+    void showCorrectAnswers()//正答数がsavedataから見れるようになってから
+    {
+        Text_Score_Sum.text = correctAnswers.ToString();
     }
 
     // Click to Go title Scene
